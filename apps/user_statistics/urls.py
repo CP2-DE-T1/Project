01eb616d2625babccc03ef685a70e483b84ac768
hssong95 <hssong95@me.com>
2023-02-03 05:31:36 +0900

    fix: statistics bug fixed

--- apps/user_statistics/urls.py
+++ apps/user_statistics/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import GenderStatistics, AgeStatistics, LoginStatistics
+from .views import GenderStatistics, AgeStatistics, BoardGenderStatistics, BoardAgeStatistics
+# from .views import LoginStatistics
 
 urlpatterns=[
     path('user/gender',GenderStatistics.as_view()),
     path('user/age',AgeStatistics.as_view()),
-    path('user/login',LoginStatistics.as_view()),
+    # path('user/login',LoginStatistics.as_view()),
+    path('board/gender', BoardGenderStatistics.as_view()),
+    path('board/age',BoardAgeStatistics.as_view())
 ]
--- apps/user_statistics/views.py
+++ apps/user_statistics/views.py
@@ -4,6 +4,7 @@ from apps.boards.models import Board
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from datetime import datetime
+# from dateutil.relativedelta import relativedelta
 
 # Create your views here.
 #남녀 별 통계
@@ -18,24 +19,53 @@ class GenderStatistics(APIView):
 #연령대 별 통계
 class AgeStatistics(APIView):
 
-    age_statistics = {}
+    
     def get(self, request):
-        year_now = datetime.now().today()
+        age_statistics = {}
+        year_now = datetime.now().year
         for i in range(1,11):
-            age_count = User.objects.filter(birth_date__year__range=(year_now-i*10,year_now+1-(i-1)*10)).count()
+            #datetime은 month, date 필수로 입력해야함
+            age_count = User.objects.filter(birth_date__range=[datetime(year_now-(i*10),1,1),datetime(year_now-(i-1)*10+1,1,1)]).count()
             if i == 1:
                 age_statistics['10대 이하']=age_count
             else:
-                age_statistics[f'{i-1}대']=age_count
+                age_statistics[f'{i-1}0대']=age_count
         return Response({'나이대 분포': age_statistics})
 
 
 #이용시간 별 통계
-class LoginStatistics(APIView):
-    login_statistics = {}
+# class LoginStatistics(APIView):
+    
+#     def get(self, request):
+#         login_statistics={}
+#         for i in range(6):
+#             h = 
+#         login_statistics['0~6']=User.objects.filter(last_login__hour__range=(0,7)).count()
+#         login_statistics['7~12']=User.objects.filter(last_login__hour__range=(7,13)).count()
+#         login_statistics['13~18']=User.objects.filter(last_login__range=(13,19)).count()
+#         login_statistics['19~24']=User.objects.filter(last_login__range=(19,24)).count()
+#         return Response({'로그인 시간 별 통계':login_statistics})
+
+#게시판 남녀 통계
+class BoardGenderStatistics(APIView):
+    
     def get(self, request):
-        login_statistics['0~6']=User.objects.filter(updated_at__time__range=(0,7))
-        login_statistics['7~12']=User.objects.filter(updated_at__time__range=(7,13))
-        login_statistics['13~18']=User.objects.filter(updated_at__time__range=(13,19))
-        login_statistics['19~24']=User.objects.filter(updated_at__time__range=(19,24))
-        return Response({'로그인 시간 별 통계':login_statistics})
+        male_board_total = Board.objects.filter(user__gender='Male').count()
+        female_board_total = Board.objects.filter(user__gender='Female').count()
+        return Response({'남자 작성 게시물 수': male_board_total, '여자 작성 게시물 수': female_board_total})
+
+#게시판 나이대 통계
+
+class BoardAgeStatistics(APIView):
+
+    def get(self, request):
+        age_statistics = {}
+        year_now = datetime.now().year
+        for i in range(1,11):
+            
+            age_count = Board.objects.select_related('user').filter(user__birth_date__range=[datetime(year_now-(i*10),1,1),datetime(year_now-(i-1)*10+1,1,1)]).count()
+            if i == 1:
+                age_statistics['10대 이하']=age_count
+            else:
+                age_statistics[f'{i-1}0대']=age_count
+        return Response({'작성자 나이대 분포': age_statistics})
