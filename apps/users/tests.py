1a69801fc73d71363195d13d3e1e31d6b097de2c
hssong95 <hssong95@me.com>
2023-02-08 04:22:18 +0900

    docs: change in requirements.txt

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,27 +1,28 @@
 from django.test import TestCase
 from rest_framework.test import APIClient, APITestCase
-from .models import User
+from apps.users.models import User
 from rest_framework import status
 # Create your tests here.
 
 #ref:https://velog.io/@migdracios/DRF-DRF-%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%BD%94%EB%93%9C-%EC%9E%91%EC%84%B1%ED%95%98%EA%B8%B0
+#!!함수 앞에 test 붙여야 함
 class TestUsersApp(APITestCase):
     # 각각 test method 실행시 마다 실행됨
     # setUpTestData()는 테스트 시작할때 딱한번 실행됨
-    def setUp(self):
-        self.user ={
-            'username':'test12',
-            'name':'test12',
-            'email':'test12@django.com',
-            'password':'sample1111',
-            'password_check':'sample1111',
-            'gender':'Female',
-            'birth_date':'2000-01-01',
-        }
-        self.user.save()
+    # def setUp(self):
+    #     self.user ={
+    #         'username':'test12',
+    #         'name':'test12',
+    #         'email':'test12@django.com',
+    #         'password':'sample1111',
+    #         'password_check':'sample1111',
+    #         'gender':'Female',
+    #         'birth_date':'2000-01-01',
+    #     }
+    #     self.user.save()
     
     #회원가입 test
-    def sign_up_test(self):
+    def test_sign_up(self):
         self.test_user={
             'username':'tester',
             'name':'tester_name',
@@ -37,7 +38,7 @@ class TestUsersApp(APITestCase):
         self.assertEqual(self.response.status_code, status.HTTP_201_Created)
     
     # 비밀번호 확인 test
-    def sign_up_validation_password(self):
+    def test_sign_up_validation_password(self):
         self.test_user={
             'username':'tester',
             'name':'tester_name',
@@ -53,7 +54,7 @@ class TestUsersApp(APITestCase):
         self.assertEqual(self.response.status_code, status.HTTP_400_BAD_REQUEST)
 
     #아이디 중복 방지 test
-    def sign_up_validation_duplicated_user_name(self):
+    def test_sign_up_validation_duplicated_user_name(self):
         self.test_user={
             'username':'tester',
             'name':'tester_name',
@@ -69,7 +70,7 @@ class TestUsersApp(APITestCase):
         self.assertEqual(self.response.status_code, status.HTTP_400_BAD_REQUEST)
 
     # 로그인 test(성공)
-    def sign_in_test(self):
+    def test_sign_in_test(self):
         self.data={
             'username':'test12',
             'passwor':'sample1111',
@@ -80,7 +81,7 @@ class TestUsersApp(APITestCase):
         self.asserEqual(self.response.status_code, status.HTTP_200_OK)
 
     # 로그인 test(실패)
-    def sign_in_test_fail(self):
+    def test_sign_in_test_fail(self):
         self.data={
             'username':'test12',
             'passwor':'sample2222',
